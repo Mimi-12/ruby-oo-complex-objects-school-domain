57e44927f99fc08a18a157b15e20b5ec93a1f4ba
Mimi-12 <oneiconforlife26@gmail.com>
2020-11-02 15:25:17 -0800

    commit tree 3f7f76f94b56bd1daea8600074330cd3e6a2c813

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+attr_accessor :roster
+    def initialize(phil)
+        @roster = {}
+    end
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+  def grade(number)
+    @roster[number]
+  end
+   def sort
+    hash = {}
+    @roster.keys.sort.each do |number|
+        hash[number] = @roster[number].sort
+    end
+    hash
+   end
+end
